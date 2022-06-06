e5b1041cd7323b0fbc3698b42da48c27fa535f63
triptocode <3580rio@gmail.com>
2022-06-06 20:05:32 +0900

    var let const / block, global scope

---
+++ variable.js
@@ -0,0 +1,43 @@
+// 변수 3개 비교: var, let, const 
+// Block Scope vs Global Scope
+
+
+// 1. var 
+// var - 과거에 사용, 현재는 let, const 권함
+// var 의 선언하지 않아도 할당되는 문제 
+// var hoisting (move declaration from bottome to top )
+// has no block scope
+{   age = 4;
+    var age;
+}
+    console.log(age);
+
+
+// 2. let
+let globalName = 'globalName'
+{
+console.log(globalName);
+
+let name = 'ana';
+console.log(name);
+
+name = 'lisa';
+console.log(name);
+
+name = 'nick';
+}
+
+console.log(name);
+console.log(globalName);
+
+
+// 3. Constants - immutable : security, tread safety, reduce human mistake
+const daysInWeek = 7;
+
+const a = 1/0;
+const b = -1/0;
+const c = 'not a num'/2;
+console.log(a);
+console.log(b);
+console.log(c);
+
