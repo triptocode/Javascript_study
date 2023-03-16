424b6db803a39d9e21e5b20f61c4113a90ed7f9e
triptocode <3580rio@gmail.com>
2023-03-16 18:43:38 +0900

    Array(3).fill(null) , undefined와 empty 차이

--- array06_fill.js
+++ array06_fill.js
@@ -1,11 +1,29 @@
 
-const squares1 = Array(3)
-const squares2 = Array(3).fill(null);
-const squares3 = Array(3).fill('데이터값');
+// Array(length=배열의요소들 갯수) : RangeError is thrown if there's only one argument Array(arrayLength) and its value is not between 0 and 2의32승 - 1 (inclusive)
+// 참고: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array
 
-console.log(squares1) // [empty × 3]
-console.log(squares2) // [null, null, null]
-console.log(squares3) // ['데이터값', '데이터값', '데이터값']
+// 1 byte  = 8 bits
+// 4 bytes = 32 bits
+
+console.log(Array(4294967295)) // [ <4294967295 empty items> ]
+// console.log(Array(4294967296)) // RangeError: Invalid array length
+
+
+const arr1 = Array(3)
+const arr2 = Array(3).fill(null);
+const arr3 = ["","",""]
+const arr4 = Array(3).fill('데이터값');
+
+console.log(arr1) //  [empty × 3]
+console.log(arr2) //  [ null, null, null]
+console.log(arr3) //  [ '', '', '' ]
+console.log(arr4) //  ['데이터값', '데이터값', '데이터값']
+
+console.log(arr1.every(Boolean)) // true
+console.log(arr2.every(Boolean)) // false
+console.log(arr3.every(Boolean)) // false
+console.log(arr4.every(Boolean)) // true
+
 
 
 
@@ -19,4 +37,13 @@ let squares6= ['x', 'x', 'x']
 console.log(squares6.every(Boolean)) // true
 
 let squares7= ['x', 'x', 'o']
-console.log(squares7.every(Boolean)) // true
+console.log(+squares7.every(Boolean)) // true
+
+let squares8= [1,2,3]
+console.log(squares8.every(Boolean))  // true
+
+
+
+
+
+//node array06_fill
