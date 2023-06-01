7a87bb5de27d89920d54a6bd66fe0c1bd460cef6
triptocode <3580rio@gmail.com>
2023-06-01 23:59:37 +0900

    a

--- loop_forEach_map.js
+++ loop_forEach_map.js
@@ -1,10 +1,25 @@
-// Example of forEach()
+// 1. forEach()
+const arr1 = [1, 2, 3];
+const myArr1 = [];
 
-const arr = [1, 2, 3, 4, 5];
-const mulArr = [];
-
-arr.forEach(num => {
-  mulArr.push(num * 3);
+arr1.forEach(num => {
+    myArr1.push(num * 3);
 });
 
-console.log(mulArr); // [3, 6, 9, 12, 15]
+console.log(myArr1); // [3, 6, 9]
+console.log(arr1)
+
+// 1-2 forEach
+const arr2 = [5,10,15];
+const myArr2= arr2.forEach(num=>num*2)
+
+console.log(myArr2); // undefined
+console.log(arr2)
+
+// 2.  map()
+const arr3 = [1,2,3];
+const myArr3 = arr3.map(num=> num*3)
+
+console.log(myArr3); // [3, 6, 9]
+console.log(arr3)
+
