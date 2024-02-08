d544cee157031ada56232e02bd43d29e661329e0
triptocode <3580rio@gmail.com>
2024-02-09 08:51:22 +0900

    JS의 오버라이딩 오버로드

--- Overload_Override/over01.js
+++ Overload_Override/over01.js
@@ -1,22 +1,24 @@
-
-var Animal = function() {
-};
-Animal.prototype.move = function() {
-  console.log('동물이 움직여요');
+let Flight = function() {
 };
 
-var Cat = function() {
-  Animal.apply(this, arguments); // 속성 상속받는 방법
+Flight.prototype.move = function() {
+  console.log('비행기 이륙');
+};
+
+let KE = function() {
+    Flight.apply(this, arguments); // 속성 상속
 }
-Cat.prototype = Object.create(Animal.prototype); // 프로토타입 상속 방법
-Cat.prototype.constructor = Cat; // 버그 패치
 
-console.log(new Animal().move()); // 동물이 움직여요
-console.log(new Cat().move()); // 동물이 움직여요
+KE.prototype = Object.create(Flight.prototype); // 프로토타입 상속 
+KE.prototype.constructor = KE; // 버그 패치
 
-Cat.prototype.move = function(sound) { // < 오버라이딩 >
-  console.log(sound + ' 움직여요');
-  return '야옹';
+    console.log(new Flight().move()); // 비행기 이륙 // new없으면 에러: console.log(Flight().move())
+    console.log(new KE().move()); //  비행기 이륙
+
+KE.prototype.move = function(flight_type) { // < 오버라이딩 >
+  console.log(flight_type + ' 이륙');
+  return '착륙';
 };
 
-console.log(new Cat().move('살금살금')); // 살금살금 움직여요 // 야옹
+    console.log(new KE().move('대한항공')); //  대한항공 이륙 // 착륙
+    console.log(new KE().move()); // undefined 이륙 // 착륙
